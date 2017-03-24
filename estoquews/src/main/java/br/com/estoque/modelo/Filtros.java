2aff75f05b2f9714f4cb7b46af6b48be69711ecd
KRATOS\doug <doareis@gmail.com>
2017-03-24 13:09:42 -0300

    versao inicial

---
+++ estoquews/src/main/java/br/com/estoque/modelo/Filtros.java
@@ -0,0 +1,20 @@
+package br.com.estoque.modelo;
+
+import java.util.List;
+
+public class Filtros {
+
+	private List<Filtro> filtros;
+
+	public Filtros(List<Filtro> filtros) {
+		this.filtros = filtros;
+	}
+
+	Filtros() {
+	}
+	
+	public List<Filtro> getLista() {
+		return filtros;
+	}
+	
+}
